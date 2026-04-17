b5e79463e287266004c29d4be3ae42138e64c230
GaelGlez <gaelos.glez@gmail.com>
2026-04-17 14:51:13 -0600

    Pruebas diseño

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { Button } from '@heroui/react';
+
 function App() {
- return ( 
-        <div>
-            <h1>Hello, World!</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <Button variant="primary"> My Buttonn </Button>
+
+      <Button variant="secondary">Edit</Button> 
+    </div>
+
+
+  );
 }
 
 export default App;
