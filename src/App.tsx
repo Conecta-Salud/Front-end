b651e155bd3940341f7ef0993cee2f5f55e2e551
GaelGlez <gaelos.glez@gmail.com>
2026-04-17 20:50:58 -0600

    CustomInputField prueba 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import { Button, Label } from '@heroui/react';
 import { Input } from '@heroui/react';
+import { useState } from 'react';
 
 function App() {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (field: string, value: string) => {
+    setForm((prev) => ({
+      ...prev,
+      [field]: value,
+    }));
+  };
+  
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello, World!</h1>
@@ -13,6 +27,16 @@ function App() {
         <Label htmlFor="input-type-password">Password</Label>
         <Input id="input-type-password" placeholder="••••••••" type="password" />
       </div>  
+
+      <CustomInputField
+        name="email"
+        label="Correo"
+        type="email"
+        placeholder="correo@ejemplo.com"
+        value={form.email}
+        onChange={(value) => handleChange("email", value)}
+        importance
+      />
         
         </div>
   );
